e27a0a37216452b9de22cac10c257cc0fd537382
janisaez@gmail.com <janisaez@gmail.com>
2014-06-25 15:33:44 +0100

    initial commit

--- js/mailer.php
+++ js/mailer.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
+        $recipient = "janisaez@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
