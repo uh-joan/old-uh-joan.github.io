586350903000677819e2ceeefcf6cd58190aa9f8
Jani <janisaez@gmail.com>
2014-08-26 19:01:26 +0100

    slide 1

--- showcase/lamundana/js/main-min.js
+++ showcase/lamundana/js/main-min.js
@@ -9,8 +9,7 @@ $(document).ready(function(){
         a.show("img/background.jpg");}
     else
     {
-        a.show("video/surf.mp4",{ambient:true,doLoop:true,altSource:"video/surf.ogv"});
-    }
+        a.show("video/surf.mp4",{ambient:true,doLoop:true,altSource:"video/surf.ogv"});}
     setTimeout(function(){$("#big-video-wrap").show().animate({opacity:1});},2500);
     $(".demo1 .rotate").textrotator({animation:"fade",speed:2000});
     $(".take-a-look").on("click",function(){
@@ -22,9 +21,9 @@ $(document).ready(function(){
         switch($(this).children().data("index"))
         {
             case 1:$(this).children().append("<i class='entypo-home'></i>");break;
-            //case 2:$(this).children().append("<i class='entypo-dot'></i>");break;
-            case 3:$(this).children().append("<i class='entypo-dot'></i>");break;
-            //case 4:$(this).children().append("<i class='entypo-dot-3'></i>");break;
+            case 2:$(this).children().append("<i class='entypo-dot'></i>");break;
+            case 3:$(this).children().append("<i class='entypo-dot-2'></i>");break;
+            case 4:$(this).children().append("<i class='entypo-dot-3'></i>");break;
             case 5:$(this).children().append("<i class='entypo-mail'></i>");break;}
     });
     setInterval(function(){
@@ -53,15 +52,24 @@ $(document).ready(function(){
                 $(".slide-2").css("background-color","#34495e");
             }
         }
-
+        if($("body").hasClass("viewing-page-4")){
+            $(".slide-3-title").removeClass("hide").addClass("animated fadeInLeft delay-1");
+            $(".slide-3-1").removeClass("hide").addClass("animated fadeInLeft delay-3");
+            $(".iphone-3").removeClass("hide").addClass("animated fadeInUp delay-3");
+            $(".slide-3-2").removeClass("hide").addClass("animated fadeInDown delay-5");
+            $(".slide-3-3").removeClass("hide").addClass("animated fadeInDown delay-6");
+            $(".slide-3-scroll").removeClass("hide").addClass("animated fadeInDown delay-12");
+            $(".icon span").css("color","#333");
+            if(c===true){$(".slide-3").css("background","#f1c40f");
+            }
+        }
         if($("body").hasClass("viewing-page-5")){
             $(".icon span").css("color","#fff");
         }
         if(c==true&&b==false){
             $(".slide-1").css("background","#e74c3c");
             $(".slide-2").css("background","#1ABC9C");
-
-            b=true;
+            $(".slide-3").css("background","#34495e");b=true;
         }
     },300);
     $(".settings-icon").on("click",function(){
@@ -77,7 +85,7 @@ $(document).ready(function(){
         $(".home").toggleClass("full-color");
         $(".slide-1").toggleClass("full-color");
         $(".slide-2").toggleClass("full-color");
-
+        $(".slide-3").toggleClass("full-color");
         $(".contact").toggleClass("full-color");
     });
     $(".toggle-phone").on("click",function(){
@@ -93,6 +101,6 @@ $(document).ready(function(){
     }
         $(".slide-1").toggleClass("fade-background");
         $(".slide-2").toggleClass("fade-background");
-        
+        $(".slide-3").toggleClass("fade-background");
     });
 });
